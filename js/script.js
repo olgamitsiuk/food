8a303ecee9369e049433437a69b3c7691501a401
Mitsiuk Olha <mitsyuk.olga@gmail.com>
2024-11-03 00:49:50 +0000

    fix timer

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer('.timer', '2024-12-24');
+    timer('.timer', '2024-12-12');
     cards();
     calc();
     forms('form', modalTimerId);
